7106b831cf8cd802c842f1f2db52136792a170e4
Shrinivas Choudhary <shrinivaschowdhary97@gmail.com>
2022-07-29 14:50:44 +0530

    Removed sqlite db file

--- learn_course/settings.py
+++ learn_course/settings.py
@@ -86,8 +86,6 @@ WSGI_APPLICATION = 'learn_course.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LearnCourseDB',
         'USER': 'api_user',
